ff51c1f83e7a2494ff898a62be63ee0959d1c32a
Khais COLIN <kh.col@orange.fr>
2020-07-06 23:14:28 +0200

    Convert old boot animation into new format
    Signed-off-by: Khais COLIN <kh.col@orange.fr>

--- src/scenes/start_computer.py
+++ src/scenes/start_computer.py
@@ -24,6 +24,8 @@ import curses
 from pathlib import Path
 from time import sleep
 
+from src.core.render.boot_animation import (BootAnimation, BootAnimationStage, StyledText,
+                                            BootAnimationStageStep, )
 from src.core.scene import Scene
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent.absolute()
@@ -44,7 +46,9 @@ class StartComputer(Scene):
     The first computer the user can use.
     """
 
-    def start(self) -> None:
+    def start(self) -> None: # pylint: disable=R0914
+        # TODO: Move the animation definiton to own file.
+        # TODO: Programming language name: Parcel-3
         """
         Shows the init sequence of the first computer.
 
@@ -55,38 +59,55 @@ class StartComputer(Scene):
             curses.color_pair(0) | curses.A_ITALIC | curses.A_BOLD | curses.A_BLINK
         )
         curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        font_info = curses.color_pair(1)
         curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        font_good = curses.color_pair(2) | curses.A_BOLD
-        font_working = font_info | curses.A_BLINK | curses.A_REVERSE
         curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-        font_bad = curses.color_pair(3) | curses.A_BOLD
 
-        self.addinto(1, 1, "EtherBIOS v2.3.1 initialising...", font_info)
-        self.addinto(45, 1, "WORKING", font_working)
-        sleep(1.5)
-        self.addinto(1, 2, "CPU 0: Ether Industries Pulse 32 Cores 128 bit 9MHz")
-        sleep(0.3)
-        self.addinto(1, 3, "CPU 1: Ether Industries Pulse 32 Cores 128 bit 9MHz")
-        sleep(0.3)
-        self.addinto(1, 4, "CPU 2: Ether Industries Pulse 32 Cores 128 bit 9MHz")
-        sleep(0.3)
-        self.addinto(1, 5, "CPU 3: Ether Industries Pulse 32 Cores 128 bit 9MHz")
-        sleep(0.5)
-        self.addinto(1, 6, "GPU 0: Ether Industries UltraText")
-        sleep(0.3)
-        self.addinto(45, 1, "[DONE] ", font_good)
-        sleep(0.1)
-
-        self.addinto(1, 7, "STARTING SELF-TEST", font_info)
-        self.addinto(45, 7, "WORKING", font_working)
-        sleep(1.5)
-        self.addinto(
-            1, 8, "WARNING No graphics available, starting in text mode", font_bad
+        # text definition
+        init = StyledText(self.renderer, "EtherBIOS v2.3.1 initialising...", 1)
+        self_test = StyledText(self.renderer, "STARTING SELF-TEST...", 1)
+        cpu0 = StyledText(
+            self.renderer, "CPU 0: Ether Industries Pulse 32 Cores 128 bit 9MHz", 0
         )
-        sleep(0.7)
-        self.addinto(45, 7, "[DONE] ", font_good)
-        self.addinto(1, 9, "Booting from disk 0...", font_info)
+        cpu1 = StyledText(
+            self.renderer, "CPU 1: Ether Industries Pulse 32 Cores 128 bit 9MHz", 0
+        )
+        cpu2 = StyledText(
+            self.renderer, "CPU 2: Ether Industries Pulse 32 Cores 128 bit 9MHz", 0
+        )
+        cpu3 = StyledText(
+            self.renderer, "CPU 3: Ether Industries Pulse 32 Cores 128 bit 9MHz", 0
+        )
+        gpu = StyledText(self.renderer, "GPU 0: Ether Industries UltraText", 0)
+        gpu_warning = StyledText(
+            self.renderer,
+            "WARNING: No graphics available, starting in text mode",
+            3,
+            bold=True,
+        )
+        boot = StyledText(self.renderer, "Booting from disk 0...", 1)
+        progress = StyledText(self.renderer, "WORKING", 1, blinking=True, inverted=True)
+        finished = StyledText(self.renderer, "DONE", 2, bold=True)
+
+        # step list definition
+        cpus = [cpu0, cpu1, cpu2, cpu3]
+        cpu_steps = [BootAnimationStageStep(cpu, delay=0.3) for cpu in cpus]
+        gpu_step = BootAnimationStageStep(gpu, delay=0.3)
+        bios_steps = cpu_steps + [gpu_step]
+        self_test_steps = [BootAnimationStageStep(gpu_warning, delay=0.7)]
+
+        # stage definitions
+        bios_stage = BootAnimationStage(
+            self.renderer, init, progress, finished, bios_steps, 1.5
+        )
+        self_test_stage = BootAnimationStage(
+            self.renderer, self_test, progress, finished, self_test_steps
+        )
+        boot_stage = BootAnimationStage(self.renderer, boot)
+
+        # BootAnimation works.
+        animation = BootAnimation(self.renderer, [bios_stage, self_test_stage, boot_stage],
+                                  delay=1.5)
+        animation.start()
 
         sleep(1.5)
 
