91fde9586419488c4756a10ebbdc4dd53faa7e72
Khais COLIN <kh.col@orange.fr>
2020-07-08 12:30:21 +0200

    Remove double-copyright
    Signed-off-by: Khais COLIN <kh.col@orange.fr>

--- src/core/boot_animation.py
+++ src/core/boot_animation.py
@@ -22,25 +22,6 @@ animations with progress updates.
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-#  This file is part of Universal Sandbox.
-#
-#  Copyright (C) © 2020 Khaïs COLIN <logistic-bot@protonmail.com>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# ------------------------------------------------------------------------------
-
 import curses
 from time import sleep
 from typing import List, Optional, Sized, Union
