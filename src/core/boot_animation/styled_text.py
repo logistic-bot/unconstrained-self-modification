093d16030bb4d0fbed40f2c25f9f3740590fe3f9
Khais COLIN <kh.col@orange.fr>
2020-07-12 13:46:49 +0200

    Remove OldStyledText
    Signed-off-by: Khais COLIN <kh.col@orange.fr>

--- src/core/boot_animation/styled_text.py
+++ src/core/boot_animation/styled_text.py
@@ -18,17 +18,13 @@
 # ------------------------------------------------------------------------------
 
 # TODO: REFACTORING BOOT_ANIMATION PLAN
-# TODO: Rewrite StyledText so that it can take a list of StyledText as its text argument and is
-#  accepted by CursesRenderer as a text.
 # TODO: Refactor Stage.
 from __future__ import annotations
 
 import curses
-from typing import Optional, Union, List, TypeVar
+from typing import Optional, Union, List
 
 from src.core.render import CursesRenderer
-
-CursesColorPairNumber = TypeVar("CursesColorPairNumber", bound=int)
 
 
 class StyledText:
@@ -40,7 +36,7 @@ class StyledText:
         self,
         renderer: Optional[CursesRenderer],
         text: Union[str, List[str], List[StyledText]],
-        color: CursesColorPairNumber,
+        color: int,
         invert: bool = False,
         blink: bool = False,
         bold: bool = False,
@@ -129,63 +125,4 @@ class StyledText:
                 "Please try using one of the supported text types."
             )
 
-        self.renderer.refresh()
-
-
-class OldStyledText:
-    """
-    This class is used to draw styled text. It abstracts away curses attributes system.
-
-    :param renderer: A renderer instance to show the styled text
-    :param text: The text to be styled
-    :param color: A curses color pair number
-
-    :param inverted: If the text colors should be inverted
-    :param blinking: If the text should be blinking
-    :param bold: If the text should be bold
-    :param italic: If the text should be italic
-    """
-
-    def __init__(
-        self,
-        renderer: Optional[CursesRenderer],
-        text: Optional[str] = None,
-        color: int = 0,
-        inverted: bool = False,
-        blinking: bool = False,
-        bold: bool = False,
-        italic: bool = False,
-    ) -> None:
-        effects = curses.A_NORMAL
-        if inverted:
-            effects = effects | curses.A_REVERSE
-        if blinking:
-            effects = effects | curses.A_BLINK
-        if bold:
-            effects = effects | curses.A_BOLD
-        if italic:
-            effects = effects | curses.A_ITALIC
-
-        self.font = curses.color_pair(color) | effects
-        self.text = text
-        self.renderer = renderer
-
-    @property
-    def draw(self) -> bool:
-        """
-        Returns true if there is something to draw
-        """
-        if self.text is None:
-            return False
-        return True
-
-    def show(self, x_pos: int, y_pos: int) -> None:
-        """
-        Show the styled text at the given position
-        :return: None
-        """
-        assert self.renderer is not None
-        if self.draw:
-            assert isinstance(self.text, str)
-            self.renderer.addtext(x_pos, y_pos, self.text, self.font)
         self.renderer.refresh()
