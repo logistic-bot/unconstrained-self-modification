5460284b1755dd5f7f092b36bfb5f5b5e09ed1a5
ToufouMaster <68186078+ToufouMaster@users.noreply.github.com>
2020-07-28 11:27:12 +0200

    Update (a verif) corrupted_login_new_save.py

--- src/scenes/corrupted_login_new_save.py
+++ src/scenes/corrupted_login_new_save.py
@@ -40,6 +40,7 @@ class CorruptedLoginNewSave(FullScreenScene):
         """
         See above
         """
+        logger = logging.getLogger(__name__)
         start_computer_scene = StartComputer(self.renderer, self.state)
         start_computer_scene.start()
 
@@ -50,12 +51,14 @@ class CorruptedLoginNewSave(FullScreenScene):
             self.renderer
         )
         password_corrupt_animation.start()
+        logger.debug("Start password_corrupt_animation")
 
         username_prompt = "New superuser name: "
         password_prompt = "New superuser password: "
         password_confirm = "Confirm new superuser password: "
 
         logged_in = False
+        logger.debug("Logged in : "+logged_in)
         username = ""
         password = ""
         while not logged_in:
@@ -67,6 +70,7 @@ class CorruptedLoginNewSave(FullScreenScene):
             if password == confirmed_password:
                 # noinspection PyUnusedLocal
                 logged_in = True
+                logger.debug("Logged in : "+logged_in)
             elif password == "":
                 self.addinto(1, 9, "Password is empty.")
             elif username == " ":
@@ -76,22 +80,31 @@ class CorruptedLoginNewSave(FullScreenScene):
 
             sleep(1)
             self.clear()
-
+        
         far_away_future = datetime.timedelta(days=365 * 126)
         save_creation = datetime.date.today() + far_away_future
+        logger.debug("Create new date_time : " + str(save_creation))
 
         self.state.data.update()
+        logger.debug("state.data updated")
         self.state.data["metadata"]["save_creation"] = str(save_creation)
+        logger.debug("state.data['metadata']['save_creation'] : "+self.state.data["metadata"]["save_creation"])
         self.state.data["metadata"]["save_date"] = str(save_creation)
+        logger.debug("state.data['metadata']['save_date'] : "+self.state.data["metadata"]["save_date"])
         self.state.data["name"] = username
+        logger.debug("state.data['name'] : "+self.state.data["name"])
         self.state.data["user"]["password"] = password
+        logger.debug("state.data['user']['password'] : "+self.state.data["user"]["password"])
         self.state.data["user"]["username"] = username
+        logger.debug("state.data['user']['username'] : "+self.state.data["user"]["username"])
         self.state.data["login"]["delay_incorrect_password"] = 1
+        logger.debug("state.data['login']['delay_incorrect_password'] : "+self.state.data["login"]["delay_incorrect_password"])
 
         self.addinto(1, 9, "[ether-login c198762] Creating new user database...")
 
         save_manager = SaveManager()
         save_manager.save_state(self.state)
+        logger.debug("New user data_base Created")
 
         self.addinto(1, 10, "[ether-login c198762] Done.")
         self.addinto(1, 12, "Welcome! Type 'help' for help!")
