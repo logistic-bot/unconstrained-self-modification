a83e4b9dd688b8e8a24cbbc743214816f0ba713f
Khais COLIN <logistic-bot@protonmail.com>
2020-07-30 18:54:28 +0200

    Add logging to state module
    Signed-off-by: Khais COLIN <logistic-bot@protonmail.com>

--- src/core/state/save_manager.py
+++ src/core/state/save_manager.py
@@ -20,6 +20,7 @@ This file contains the SaveManager class, which manages a group of Saves
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ------------------------------------------------------------------------------
+import logging
 from typing import List
 
 from src import GAME_ROOT_DIR
@@ -30,6 +31,8 @@ SAVEFILE_EXTENSION = ".json"
 SAVE_DIRECTORY = GAME_ROOT_DIR / "saves"
 SAVE_DIRECTORY.mkdir(exist_ok=True)
 
+logger = logging.getLogger(__name__)
+
 
 class SaveManager:
     """
@@ -38,21 +41,27 @@ class SaveManager:
 
     def __init__(self) -> None:
         self.save_dir = SAVE_DIRECTORY
+        logger.info("Creating new SaveManager with save dir '%s'", self.save_dir)
 
     @property
     def saves(self) -> List[GameState]:
         """
         Return an unordered (as in: in no particular order) list of all saves in self.save_dir
         """
+        logger.info("Getting save list")
+
         saves = []
         for savefile_path in self.save_dir.iterdir():
             if savefile_path.suffix == SAVEFILE_EXTENSION:
+                logger.info("Found save file at '%s", savefile_path)
+
                 save = GameState()
                 save.load(savefile_path)
                 saves.append(save)
             else:
-                pass  # TODO: When logging is added, log a warning
+                logger.warning("Found non-savefile file at '%s'", savefile_path)
 
+        logger.info("All saves: '%s'", saves)
         return saves
 
     def save_state(self, state: GameState) -> None:
@@ -63,7 +72,13 @@ class SaveManager:
         :param state: The state to save
         :return: None
         """
+
+
         path = self.save_dir / f"{state.data['name']}.json"
+
+        logger.info("saving state of GameState '%s' at file '%s'", state, path)
+
+        # TODO: See if this is still needed, maybe replace by state.save(path)
         state_duplicate = GameState()
         state_duplicate.data = state.data
         state_duplicate.save(path)
